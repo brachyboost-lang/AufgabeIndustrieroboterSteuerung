023dcc998059ae882da3443c098783c087519fc4
Alexander Lang <Alexander.Lang@it-lang.de>
2026-03-24 10:37:38 +0100

    Interface hinzugefügt

---
+++ AufgabeIndustrieroboterSteuerung/Repos/IRoboterVerwaltung.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AufgabeIndustrieroboterSteuerung.Repos
+{
+    internal interface IRoboterVerwaltung
+    {
+        void AddRoboter(Industrieroboter roboter);
+        // void RemoveRoboter(Industrieroboter roboter);
+        void UpdateRoboter(Industrieroboter roboter);
+        // void ReadRoboter(Industrieroboter roboter);
+        // IEnumerable<Industrieroboter> GetAllRoboter();
+
+    }
+}
